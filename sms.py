9817b86faed4138492d0f7242f603990c10b5ee9
Ruairi Newman <ruairi@centosvmw.ruairinet>
2015-02-19 13:50:13 +0000

    indentation fixes and vim ts added

--- sms.py
+++ sms.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# vim:ts=4
+
 import requests, sys
 
 baseurl = "http://api.clickatell.com"
@@ -14,8 +16,8 @@ sessRequest = ("/http/auth/?user=%s&password=%s&api_id=%s&callback=%d" % (user, 
 auth = requests.get(baseurl + sessRequest)
 session_id = auth.text.split(' ', 1)[1]
 if (auth.text.split(':', 1)[0] != 'OK'):
-    print "auth failure"
-    sys.exit(1)
+	print "auth failure"
+	sys.exit(1)
 
 sendPayload = {'session_id': session_id, 'to': sys.argv[1], 'text': sys.argv[2], 'from': sender_id}
 send_message = requests.get(sendurl, params=sendPayload)
@@ -24,5 +26,5 @@ if (send_message.status_code == 200):
 	print send_message.text
 	sys.exit(0)
 else:
-    print send_message.text
-    sys.exit(1)
+	print send_message.text
+	sys.exit(1)
